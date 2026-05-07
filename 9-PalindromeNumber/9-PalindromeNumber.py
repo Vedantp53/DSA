f3883639bf6e6f707453a6af0705fddb71e125e3
Vedant Pangudwale <vedantpangudwale@gmail.com>
2026-05-08 03:50:14 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -0,0 +1,24 @@
+# Last updated: 5/8/2026, 3:50:14 AM
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        # 0 is also a palindrome
+        if x == 0:
+            return True
+
+        div = 1
+        while x >= 10 * div:
+            div *= 10
+
+        while div > 0:
+            right = x % 10
+            left = x // div
+
+            if left != right:
+                return False
+
+            x = (x % div) // 10
+            div //= 100
+
+        return True
